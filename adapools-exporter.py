2c0e0b8278cba9955b99a993c9a3a3d90d1ad696
mbos01 <mbos01@gmail.com>
2020-12-08 17:49:35 +0100

    adapools-exporter.py

--- adapools-exporter.py
+++ adapools-exporter.py
@@ -1,52 +1,69 @@
 import requests, json, time, os
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-from threading import Thread
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib import parse
 
 ### SETTINGS ####################################################################################
-sec = 600 #scrape every ..
-url = "https://js.adapools.org/pools/!!!!!YOUR-POOL-ID!!!!!/summary.json" #adapools json
 delkeys = "hist_bpe,handles,hist_roa,db_ticker,db_name,db_url,ticker_orig,pool_id,db_description,direct,stake_x_deleg,group_basic" #no metric data
 http_port = 8000 #webserver port
-http_address = "localhost" #webserver listening on
-prefix = "adapools_" #prefix for use in prometheus
+http_address = "localhost" #webserver is listening on this address
 #################################################################################################
 
-#fetch the pool data
-def getPoolData(url, delkeys):
-	pooldata = []
-	try:
-		r = json.loads(requests.get(url).text)
-	except:
-		pooldata.append("Error scraping pooldata")
-		return pooldata
+def makeHandler(url, delkeys):
+    class getHandler(BaseHTTPRequestHandler):
+        def getPoolData(self, url, delkeys, prefix): #fetch adapools data
+            pooldata = []
+            try:
+                r = json.loads(requests.get(url).text)
+            except:
+                pooldata.append("Error scraping pooldata")
+                return pooldata
 
-	delkeys = (delkeys).split(",")
-	for key in r["data"]:
-		if key not in delkeys:
-			a = str(key)
-			b = str(r["data"][key])
-			pooldata.append(prefix + a + " " + b.lower().replace("none", "0"))
+            delkeys = (delkeys).split(",") #exclude these
+            for key in r["data"]:
+                if key not in delkeys:
+                    a = str(key)
+                    b = str(r["data"][key])
+                    pooldata.append(prefix + a + " " + b.lower().replace("none", "0"))
 
-	return pooldata #return list
+            return pooldata #return list
 
-#create the metrics folder if it does not exist
-if not os.path.exists("metrics"):
-	os.makedirs("metrics")
+        def do_GET(self):
+            parsed_path = parse.urlparse(self.path) #parse url
 
-#spawn webserver to serve the metrics
-httpd = HTTPServer((http_address, http_port), SimpleHTTPRequestHandler)
-thread = Thread(target = httpd.serve_forever)
-thread.daemon = True
-thread.start()
+            if (len(parsed_path.query.split("poolid=")) < 2):
+                #there's no pool id
+                print("No pool id!")
+                return
+            else:
+                poolid = parsed_path.query.split("poolid=")[1].split("+")[0]
 
-#scrape adapools
-starttime = time.time()
+                #is prefix sent along?
+                if (len(parsed_path.query.split("prefix=")) < 2): #no custom prefix
+                    prefix = "adapools_"
+                else:
+                    prefix = parsed_path.query.split("prefix=")[1] #use custom prefix
+            
+            pooldata = self.getPoolData(url.replace("[POOLID]", poolid), delkeys, prefix)
+            html = '\r\n'.join(pooldata)
+
+			#send output to client
+            self.send_response(200)
+            self.send_header('Content-Type',
+                            'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(html.encode('utf-8'))
+
+            return
+    return getHandler
+
+#no need to change these!!
+url = "https://js.adapools.org/pools/[POOLID]/summary.json" #adapools json
+delkeys = "hist_bpe,handles,hist_roa,db_ticker,db_name,db_url,ticker_orig,pool_id,db_description,direct,stake_x_deleg,group_basic" 
+
 try:
-	while True:
-		with open("metrics/index.html", "w") as f:
-			for item in getPoolData(url, delkeys):
-				f.write("%s\n" % item)
-
-		time.sleep(sec - ((time.time() - starttime) % sec))
+	#spawn webserver
+	httpd = HTTPServer((http_address, http_port), makeHandler(url, delkeys))
+	print("Serving Adapools metrics at " + str(http_address) + ":" + str(http_port))
+	httpd.serve_forever()
 except KeyboardInterrupt:
 	httpd.shutdown()
